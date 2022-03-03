16084b05a2411bf724b64264271a2ef26d73ef65
omagarwal25 <lidagarwal@gmail.com>
2022-03-03 23:08:38 +0000

    fixed formatting

--- src/stores/games.ts
+++ src/stores/games.ts
@@ -33,7 +33,7 @@ export const useGamesStore = defineStore('games', {
     export(): Blob {
       const data = this.games.reduce(
         (e, c) => (c.data == '' ? e : e + c.data + '\r\n'),
-        'apron,human player low,human player high,intake floor, intake human,missed,scored high,scored low,preloaded balls,defense?,penalties?,intake floor, intake human,missed,scored high,scored low,climb height,climb success,match number,match type,scout id,team color,team number,rank,scout initials\r\n'
+        'apron,human player low,human player high,auto intake floor,auto intake human,auto missed,auto scored high,auto scored low,preloaded balls,defense?,penalties?,tele intake floor,tele intake human,tele missed,tele scored high,tele scored low,climb height,climb success,match number,match type,scout id,team color,team number,rank,scout initials\r\n'
       );
       // this.games.forEach((e) => (e.exported = true));
       return new Blob([data]);
